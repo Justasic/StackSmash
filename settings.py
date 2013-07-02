168b740f4d5827954b718389dde5b89f1407ef1f
Justin Crawford <Justasic@Gmail.com>
2013-07-01 23:57:18 -0700

    Added some pages and adjust settings.py
    - Added posts.html for a full list of posts that will be newest first and oldest last
    - Added a post.html for detailed display of the post (comments, etc.)
    - Added some various settings to settings.py for database and such

--- settings.py
+++ settings.py
@@ -5,16 +5,17 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
+	('Justin Crawford', 'Justasic@gmail.com')
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'StackSmash',                      # Or path to database file if using sqlite3.
+        'USER': 'ss',                      # Not used with sqlite3.
+        'PASSWORD': '<hehe u no know me>',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -27,7 +28,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
