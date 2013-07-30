06734a7d86035f5eba6658fb27e42a667ea115b4
Justin Crawford <justinc@pci-online.net>
2013-07-30 12:54:22 -0700

    Added pygments.com's 'native' syntax theme as well as set the settings for the django project.

--- StackSmash/settings.py
+++ StackSmash/settings.py
@@ -1,20 +1,24 @@
 # Django settings for StackSmash project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+cwd = os.getcwd()
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
+      ('Justin Crawford', 'Justasic@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'StackSmash',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
+        'USER': 'StackSmash',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
@@ -29,7 +33,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -72,6 +76,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    cwd + "/static/",
 )
 
 # List of finder classes that know how to find static files in
@@ -111,6 +116,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    cwd + "/templates/",
 )
 
 INSTALLED_APPS = (
