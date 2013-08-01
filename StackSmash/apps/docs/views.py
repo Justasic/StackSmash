7c590f91c4a5bc04d3c850cc2a3c2d8ccf445ca3
Justin Crawford <Justasic@Gmail.com>
2013-07-31 19:50:27 -0700

    Make source highlighting work without crashing (still need to learn how to use it)

--- StackSmash/apps/docs/views.py
+++ StackSmash/apps/docs/views.py
@@ -2,10 +2,47 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.http import Http404, HttpResponseRedirect
 from django.conf import settings
+from docutils import nodes
 from docutils.core import publish_parts
+from docutils.parsers.rst import directives, Directive
+from docutils.core import publish_cmdline, default_description
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name, TextLexer
+from pygments.formatters import HtmlFormatter
 from django.core.cache import cache
 from django.template import loader
 import os
+
+class Pygments(Directive):
+    """ Source code syntax hightlighting for ReST syntax."""
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = True
+    option_spec = {
+        'linenos': directives.flag,
+        'emphasize-lines': directives.unchanged_required,
+    }
+    has_content = True
+
+    def run(self):
+        self.assert_has_content()
+        try:
+            lexer = get_lexer_by_name(self.arguments[0])
+        except ValueError:
+            # no lexer found - use the text one instead of an exception
+            lexer = TextLexer()
+        args = {'noclasses': False}
+        if 'linenos' in self.options:
+            args['linenos'] = 'table'
+        if 'emphasize-lines' in self.options:
+            args['hl_lines'] = self.options['emphasize-lines'].split(',')
+        formatter = HtmlFormatter(**args)
+        parsed = highlight(u'\n'.join(self.content), lexer, formatter)
+        return [nodes.raw('', parsed, format='html')]
+
+# Add syntax highlighting to code blocks
+directives.register_directive('sourcecode', Pygments)
+directives.register_directive('code', Pygments)
 
 def plaintext(request, path):
 	filePath = os.path.join(settings.DOC_PATH, path)
@@ -23,7 +60,7 @@ def plaintext(request, path):
 # file suffix is .rst, otherwise print as plain text.
 def page(request, path):
     key = 'wiki.doc.page.%s' % (path.replace('/', '.'))
-    ctx = cache.get(key)
+    ctx = False #cache.get(key)
     if not ctx:
 
         filePath = os.path.join(settings.DOC_PATH, path)
@@ -41,6 +78,8 @@ def page(request, path):
 	# Could not resolve anything, 404.
         if not os.path.isfile(filePath):
             raise Http404("Path: " + filePath)
+
+
 
         ctx = {
             'parts': publish_parts(
--- StackSmash/urls.py
+++ StackSmash/urls.py
@@ -9,8 +9,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'StackSmash.views.home', name='home'),
     # url(r'^StackSmash/', include('StackSmash.foo.urls')),
 
-    url(r'^$', include('StackSmash.apps.blog')),
-    url(r'^doc/', include('StackSmash.apps.docs')),
+    url(r'^$', include('StackSmash.apps.blog.urls')),
+    url(r'^docs/', include('StackSmash.apps.docs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
