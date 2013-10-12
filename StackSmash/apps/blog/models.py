110f8bdc430e62c935353628a6fa164b9d103e3a
Justin Crawford <Justasic@Gmail.com>
2013-10-12 13:53:34 -0700

    Posting functionality is fully functional!
    - Added ImageTranslator for reStructuredText image urls
    - Turns out render functions in models are not executed.. now I need to find a better way of doing rst in posts
    - Fixed date based unique urls actually being unique based on the date
    - Posts are now properly ordered by date

--- StackSmash/apps/blog/models.py
+++ StackSmash/apps/blog/models.py
@@ -4,43 +4,168 @@ from django.core.cache import cache
 from docutils.core import publish_doctree, Publisher
 from docutils.readers import doctree
 from docutils.io import DocTreeInput, StringOutput
+from docutils import nodes
+import sys
+
+class ImageTranslator(nodes.SparseNodeVisitor):
+    """This Docutils translator implements support for a new type of
+	image element with a numeric URL, referring to an image managed
+	by our cia.apps.images app. Examples:
+
+	A full-size image:
+
+	.. image:: #123
+
+	The default thumbnail:
+
+	.. image:: #123/
+
+	A 64-pixel thumbnail:
+
+	.. image:: #123/64
+
+
+	A 256-pixel thumbnail, floating in a box:
+
+	.. image:: #123/256
+	:class: float
+
+	We include in-line styles for such images, so that we can still
+	float them when we're rendered without doc.css (for example,
+	via RSS).
+	"""
+
+    styles = {
+        'float': ('div', 'float: right; margin: 0 0 1em 1em; clear: right;'),
+        'centered': ('div', 'text-align: center;'),
+        'dashed': ('img', 'border: 1px dashed #888; padding: 2px;'),
+        'framed': ('img', 'border: 1px solid black; padding: 0;'),
+        }
+
+    def __init__(self, document):
+        nodes.SparseNodeVisitor.__init__(self, document)
+        self.images = []
+    
+    def visit_image(self, node):
+        m = re.match(r"^#(\d+)(/(\d*))?$", node['uri'])
+        if not m:
+            return
+        image_id, use_thumbnail, thumbnail_size = m.groups()
+
+        try:
+            image = ImageSource.objects.get(pk=image_id)
+        except ImageSource.DoesNotExist:
+            err = self.document.reporter.error("Image ID %s does not exist" % image_id)
+            node.replace_self(err)
+            return
+
+        try:
+            if use_thumbnail:
+                # Use a thumbnail, and link to the full image
+                instance = image.get_thumbnail(int(thumbnail_size or 128))
+                href = image.get_original().get_url()
+            else:
+                # No link, put the whole thing in directly
+                instance = image.get_original()
+                href = None
+        except ImageInstance.DoesNotExist:
+            err = self.document.reporter.error("Image not available in the specified size")
+            node.replace_self(err)
+            return
+
+        # Apply all of the image's styles. Some styles go on the
+        # image itself, some go on a containing div.
+
+        styles = {}
+        for c in node.get('classes'):
+            if c in self.styles:
+                element, style = self.styles[c]
+                styles[element] = styles.get(element, '') + style
+
+        # If we got this far, there are no more potential user errors.
+        # Store this image in our list of images to reference.
+
+        self.images.append(image)
+
+        # Build some raw HTML output, which will replace this image in the doc tree
+
+        parts = []
+
+        if 'div' in styles:
+            parts.append('<div style="%s">' % styles['div'])
+        if href:
+            parts.append('<a href="%s">' % href)
+
+        parts.append('<img src="%s" width="%d" height="%d" style="%s" />' % (
+            instance.get_url(),
+            instance.width, instance.height,
+            styles.get('img', ''),
+            ))
+    
+        if href:
+            parts.append('</a>')
+        if 'div' in styles:
+            parts.append('</div>')
+
+        node.parent.replace(node, nodes.raw(text=''.join(parts), format='html'))
+
 
 class Post(models.Model):
-	pub_date = models.DateTimeField(auto_now=True, db_index=True)
+	pub_date = models.DateTimeField(db_index=True)
 	posted_by = models.ForeignKey(User)
 	listed = models.BooleanField('Listed to public?', default=False)
 	slug = models.SlugField('slug', unique_for_date='pub_date', db_index=True)
 	title = models.CharField(max_length=100)
 	content = models.TextField()
 
-	class Meta:
-		ordering = ['-pub_date']
-		unique_together = (('slug', 'pub_date'),)
+	def __unicode__(self):
+		return self.title
 
 	def invalidate_cache(self):
 		cache.delete('ss.apps.blog.%d' % self.id)
 
-	def get_url(self):
+	#@models.permalink
+	def get_absolute_url(self):
 		return '/blog/%04d/%02d/%s/' % (self.pub_date.year, self.pub_date.month, self.slug)
 
-	def render(self):
+	def Render(self):
 		key = 'ss.apps.blog.%d' % self.id
 	#	parts = cache.get(key)
-		parts = None
+		parts = False
+	#	sys.stderr.write("Herp")
 		if not parts:
+
+			#parts = publish_parts(
+			#	source = self.content,
+			#	writer_name = "html4css1",
+			#	settings_overrides = {
+			#	    'cloak_email_addresses': True,
+			#	    'initial_header_level': 2,
+			#	},
+			#),
+
 			document = publish_doctree(source = self.content)
 			
+			visitor = ImageTranslator(document)
+			document.walkabout(visitor)
 
 			reader = doctree.Reader(parser_name='null')
-			pub = Publisher(reader, None, None, source = DocTreeInput(document), destination_class = StringOutput)
-			pub_set_writer('html4css1')
-			pub.process_programmatic_settings(None, { 'cloak_email_addresses' : True, 'initial_header_level': 2 }, None)
+			pub = Publisher(reader, None, None,
+					source = DocTreeInput(document),
+					destination_class = StringOutput)
+
+			pub.set_writer('html4css1')
+			pub.process_programmatic_settings(None, {
+				'cloak_email_addresses' : True,
+				'initial_header_level': 2 },
+				None)
+
 			pub.publish()
 			parts = pub.writer.parts
 			parts.posted_by = posted_by
 
 			#cache.set(key, parts)
-		return parts;
+		#return parts;
 
 
 	class Admin:
--- StackSmash/apps/blog/views.py
+++ StackSmash/apps/blog/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, render_to_response
 from django.http import HttpResponse
+from django.template.context import RequestContext
 from StackSmash.apps.blog.models import Post
+from StackSmash.apps.docs.views import page
 
 def index(request):
 	# get the blog posts that are published, order by date
-	posts = Post.objects.filter(listed=True).order_by('pub_date')
+	posts = Post.objects.filter(listed=True)
+
+	ctx = RequestContext(request, {
+		'posts': posts.order_by('-pub_date'),
+		})
+
 	# render to template
-	return render(request, 'blog/posts.html', {'posts': posts})
+	return render_to_response('blog/posts.html', ctx)
 
 
 def post(request, year, month, slug):
 	# Get post object
-	post = get_object_or_404(Post, slug=slug)
+	post = get_object_or_404(Post, slug = slug,
+				 pub_date__year = int(year),
+				 pub_date__month = int(month))
 	# Render to template
 	return render(request, 'blog/post.html', {'post': post})
 
@@ -20,4 +29,5 @@ def archives(request, year, month):
 	return HttpResponse('Yay!')
 
 def about(request):
-	return HttpResponse('Yay!')
+	# just use the docs view from docs/, it's much easier than making a custom view
+	return page(request, 'aboutme')
