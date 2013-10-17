8ec94802f4416b0d0e02e1ace93106015f43e67a
Justin Crawford <Justasic@Gmail.com>
2013-10-17 15:26:39 -0700

    Fix pygments integration with restructuredtext
    Pygments can now properly do syntax highlighting as long as the source block is indented.
    This allows syntax highlighting both in the documentation section, about me, and blog posts
    as long as `code` or `sourcecode` directive is specified with the source type.

--- StackSmash/apps/docs/views.py
+++ StackSmash/apps/docs/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.http import Http404, HttpResponseRedirect
 from django.conf import settings
-from docutils import nodes
 from docutils.core import publish_parts
+from docutils.core import publish_cmdline
+from docutils import nodes
 from docutils.parsers.rst import directives, Directive
-from docutils.core import publish_cmdline, default_description
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name, TextLexer
 from pygments.formatters import HtmlFormatter
@@ -13,32 +13,40 @@ from django.core.cache import cache
 from django.template import loader
 import os
 
-class Pygments(Directive):
-    """ Source code syntax hightlighting for ReST syntax."""
-    required_arguments = 1
-    optional_arguments = 0
-    final_argument_whitespace = True
-    option_spec = {
-        'linenos': directives.flag,
-        'emphasize-lines': directives.unchanged_required,
-    }
-    has_content = True
+# Options
+# ~~~~~~~
 
-    def run(self):
-        self.assert_has_content()
-        try:
-            lexer = get_lexer_by_name(self.arguments[0])
-        except ValueError:
-            # no lexer found - use the text one instead of an exception
-            lexer = TextLexer()
-        args = {'noclasses': False}
-        if 'linenos' in self.options:
-            args['linenos'] = 'table'
-        if 'emphasize-lines' in self.options:
-            args['hl_lines'] = self.options['emphasize-lines'].split(',')
-        formatter = HtmlFormatter(**args)
-        parsed = highlight(u'\n'.join(self.content), lexer, formatter)
-        return [nodes.raw('', parsed, format='html')]
+# Set to True if you want inline CSS styles instead of classes
+INLINESTYLES = False
+
+# The default formatter
+DEFAULT = HtmlFormatter(noclasses=INLINESTYLES)
+
+# Add name -> formatter pairs for every variant you want to use
+VARIANTS = {
+     'linenos': HtmlFormatter(noclasses=INLINESTYLES, linenos=True),
+}
+
+class Pygments(Directive):
+	""" Source code syntax hightlighting.
+	"""
+	required_arguments = 1
+	optional_arguments = 0
+	final_argument_whitespace = True
+	option_spec = dict([(key, directives.flag) for key in VARIANTS])
+	has_content = True
+
+	def run(self):
+		self.assert_has_content()
+		try:
+			lexer = get_lexer_by_name(self.arguments[0])
+		except ValueError:
+			# no lexer found - use the text one instead of an exception
+			lexer = TextLexer()
+		# take an arbitrary option if more than one is given
+		formatter = self.options and VARIANTS[self.options.keys()[0]] or DEFAULT
+		parsed = highlight(u'\n'.join(self.content), lexer, formatter)
+		return [nodes.raw('', parsed, format='html')]
 
 # Add syntax highlighting to code blocks
 directives.register_directive('sourcecode', Pygments)
@@ -60,7 +68,8 @@ def plaintext(request, path):
 # file suffix is .rst, otherwise print as plain text.
 def page(request, path):
 	key = 'ss.doc.page.%s' % (path.replace('/', '.'))
-	ctx = cache.get(key)
+	#ctx = cache.get(key)
+	ctx = False
 	if not ctx:
 
 		filePath = os.path.join(settings.DOC_PATH, path)
--- StackSmash/apps/lib/templatetags/restructuredtext_filter.py
+++ StackSmash/apps/lib/templatetags/restructuredtext_filter.py
@@ -11,32 +11,40 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-class Pygments(Directive):
-    """ Source code syntax hightlighting for ReST syntax."""
-    required_arguments = 1
-    optional_arguments = 0
-    final_argument_whitespace = True
-    option_spec = {
-        'linenos': directives.flag,
-        'emphasize-lines': directives.unchanged_required,
-    }
-    has_content = True
+# Options
+# ~~~~~~~
 
-    def run(self):
-        self.assert_has_content()
-        try:
-            lexer = get_lexer_by_name(self.arguments[0])
-        except ValueError:
-            # no lexer found - use the text one instead of an exception
-            lexer = TextLexer()
-        args = {'noclasses': False}
-        if 'linenos' in self.options:
-            args['linenos'] = 'table'
-        if 'emphasize-lines' in self.options:
-            args['hl_lines'] = self.options['emphasize-lines'].split(',')
-        formatter = HtmlFormatter(**args)
-        parsed = highlight(u'\n'.join(self.content), lexer, formatter)
-        return [nodes.raw('', parsed, format='html')]
+# Set to True if you want inline CSS styles instead of classes
+INLINESTYLES = False
+
+# The default formatter
+DEFAULT = HtmlFormatter(noclasses=INLINESTYLES)
+
+# Add name -> formatter pairs for every variant you want to use
+VARIANTS = {
+     'linenos': HtmlFormatter(noclasses=INLINESTYLES, linenos=True),
+}
+
+class Pygments(Directive):
+	""" Source code syntax hightlighting.
+	"""
+	required_arguments = 1
+	optional_arguments = 0
+	final_argument_whitespace = True
+	option_spec = dict([(key, directives.flag) for key in VARIANTS])
+	has_content = True
+
+	def run(self):
+		self.assert_has_content()
+		try:
+			lexer = get_lexer_by_name(self.arguments[0])
+		except ValueError:
+			# no lexer found - use the text one instead of an exception
+			lexer = TextLexer()
+		# take an arbitrary option if more than one is given
+		formatter = self.options and VARIANTS[self.options.keys()[0]] or DEFAULT
+		parsed = highlight(u'\n'.join(self.content), lexer, formatter)
+		return [nodes.raw('', parsed, format='html')]
 
 # Add syntax highlighting to code blocks
 directives.register_directive('sourcecode', Pygments)
