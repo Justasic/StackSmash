88da47a588aa6767c73346f8848eb35d31f608c5
Justin Crawford <Justasic@Gmail.com>
2013-11-25 01:59:36 -0800

    This is much easier than iterating the list with a for loop. Thanks CIA for showing me this.

--- db_cleanup.py
+++ db_cleanup.py
@@ -11,17 +11,21 @@
 import os, datetime
 
 def clean_up():
-       # Set Django settings module.
-       os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StackSmash.settings")
+	# Set Django settings module.
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StackSmash.settings")
 
-       # import our blog stuff
-       from StackSmash.apps.blog.models import Comment, Post
-       comments = Comment.objects.filter(listed=False).order_by("created")
+	# import our blog stuff
+	from StackSmash.apps.blog.models import Comment, Post
+	
+	# if comments are not listed and older than a week, delete them.
+	Comment.objects.filter(listed=False, created__lt = datetime.datetime.now() - datetime.timedelta(days=7)).delete()
+
+#       comments = Comment.objects.filter(listed=False).order_by("created")
 
        # If comments are older than a day, delete them.
-       for comment in comments:
-               if comment.created.day < datetime.datetime.now().day:
-                       Comment.objects.filter(listed=False, id=comment.id).delete()
+#       for comment in comments:
+#               if comment.created.day < datetime.datetime.now().day:
+#                       Comment.objects.filter(listed=False, id=comment.id).delete()
 
 
 if __name__ == "__main__":
